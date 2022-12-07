5b954af12198db3c9592f7bc1f17da19fc8b3ba6
Luiz Felipe <lfelipecx@gmail.com>
2022-12-07 11:03:24 -0300

    estrutura do dashboard projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from 'react-router-dom'
 
+import styles from './Projects.module.css'
+
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
 
 function Projects(){
 
@@ -9,11 +13,20 @@ function Projects(){
     if(location.state){
         message = location.state.message
     }
-    
+
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
+
             {message && <Message type="success" msg={message} />}
+
+            <Container customClass="start">
+                <p>Projetos.....</p>
+
+            </Container>
         </div>
 
     )
