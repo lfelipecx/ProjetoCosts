b3c65aa372b54280db9979274a19f01eeb93037a
Luiz Felipe <lfelipecx@gmail.com>
2022-12-12 16:26:54 -0300

    editar projeto

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,7 +5,7 @@ function Select({text, name, options, handleOnChange, value}){
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name} onChange={handleOnChange} value={value || ''} required>
-                <option>Selecione uma opção</option>
+                <option value="">Selecione uma opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>
                         {option.name}
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,12 +4,16 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import Message from '../layout/Message'
+import ProjectForm from '../project/ProjectForm'
 
 function Project() {
     const { id } = useParams()
     
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(()=> {
 
@@ -29,6 +33,31 @@ function Project() {
         },1000)
     },[id])
 
+    function editPost(project){
+        //budget validaton
+        if(project.budget < project.cost){
+            setMessage('O orçamentoo não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH', //PATCH atualiza somente o que for mudado diferente do update que atualiza tudo
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(project)//Enviando os dados
+        })
+            .then(resp => resp.json())
+            .then((data) => {
+                setProject(data)
+                setShowProjectForm(false)
+                setMessage('Projeto editado com sucesso!')
+                setType('success')
+            })
+            .catch(err => console.log(err))
+    }
+
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
     }
@@ -38,6 +67,7 @@ function Project() {
             {project.name ? (
                 <div className={styles.project_details}>
                     <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
                         <div className={styles.details_container}>
                             <h1>Projeto: {project.name}</h1>
                             <button className={styles.btn} onClick={toggleProjectForm}>
@@ -57,7 +87,11 @@ function Project() {
                                 </div>
                             ) : (
                                 <div className={styles.project_info}>
-                                    <p>detalhes do projeto para edição</p>
+                                    <ProjectForm 
+                                        handleSubmit={editPost}
+                                        btnText='Concluir Edição'
+                                        projectData={project}
+                                    />
                                 </div>
                             )}
                         </div>
